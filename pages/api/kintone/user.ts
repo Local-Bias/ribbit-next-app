5f54fa4026e8404c54c7cd467a4544ae2b08f22d
Ribbit <echo07200503@gmail.com>
2022-02-16 18:08:44 +0900

    ⚡ FireStoreとRealtime Databaseの並行運用から、Realtime Database一本に

--- pages/api/kintone/user.ts
+++ pages/api/kintone/user.ts
@@ -1,7 +1,5 @@
-import { child, get, getDatabase, ref, set, update } from "firebase/database";
-import { doc, increment, runTransaction } from "firebase/firestore";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getDB } from "../../../src/firebase";
+import { get, getDatabase, ref, set, update } from 'firebase/database';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   result: string;
@@ -10,27 +8,21 @@ type Data = {
 type ExpectedRequestBody = Partial<{
   hostname: string;
   pluginNames: string[];
-  name: "";
+  name: '';
   counter: number;
   installDate: string;
 }>;
 
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
       const body: ExpectedRequestBody = JSON.parse(req.body);
 
       let causedError = false;
       try {
-        await updateFirestore(body);
-      } catch (error) {
-        console.error("firestore更新時にエラーが発生しました", error);
-        causedError = true;
-      }
-      try {
         await updateRtdb(body);
       } catch (error) {
-        console.error("realtime database更新時にエラーが発生しました", error);
+        console.error('realtime database更新時にエラーが発生しました', error);
         causedError = true;
       }
       if (!causedError) {
@@ -43,63 +35,20 @@ export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       }
     }
   } catch (e) {
-    res
-      .status(500)
-      .json({ result: `予期せぬエラーが発生しました。${JSON.stringify(e)}` });
-    throw "API実行時にエラーが発生しました";
+    res.status(500).json({ result: `予期せぬエラーが発生しました。${JSON.stringify(e)}` });
+    throw 'API実行時にエラーが発生しました';
   }
-};
-
-const updateFirestore = async (body: ExpectedRequestBody) => {
-  const db = getDB();
-
-  const hostname = body.hostname || "___unknown";
-
-  const ref = doc(db, "kintone-plugin-users", hostname);
-  // const summaryRef = doc(db, "kintone-plugin-users", "!summary");
-
-  await runTransaction(db, async (transaction) => {
-    const doc = await transaction.get(ref);
-
-    if (!doc.exists()) {
-      await transaction.set(ref, getNewProps(hostname, body));
-      return;
-    }
-
-    const data = doc.data();
-
-    const pluginNames = body.pluginNames || [];
-    const registered: string[] = data.pluginNames || [];
-
-    const noChanges = pluginNames.every((plugin) =>
-      registered.includes(plugin)
-    );
-
-    const base = {
-      counter: increment(1),
-      lastModified: new Date(),
-    };
-
-    if (noChanges) {
-      await transaction.update(ref, base);
-    } else {
-      await transaction.update(ref, {
-        pluginNames: [...new Set([...pluginNames, ...registered])],
-        ...base,
-      });
-    }
-  });
 };
 
 const updateRtdb = async (body: ExpectedRequestBody) => {
   const db = getDatabase();
 
-  const hostname = body.hostname || "___unknown";
+  const hostname = body.hostname || '___unknown';
 
   const formattedHostname = hostname
-    .replace(".cybozu.com", "")
-    .replace(".kintone.com", "")
-    .replace(/\./g, "_dot_");
+    .replace('.cybozu.com', '')
+    .replace('.kintone.com', '')
+    .replace(/\./g, '_dot_');
 
   const reference = ref(db, `kintone/users/${formattedHostname}`);
 
@@ -114,13 +63,11 @@ const updateRtdb = async (body: ExpectedRequestBody) => {
     const pluginNames = body.pluginNames || [];
     const registered: string[] = data.pluginNames || [];
 
-    const noChanges = pluginNames.every((plugin) =>
-      registered.includes(plugin)
-    );
+    const noChanges = pluginNames.every((plugin) => registered.includes(plugin));
 
     const base = {
       counter: counter + 1,
-      lastModified: new Date().toLocaleString("ja-JP"),
+      lastModified: new Date().toLocaleString('ja-JP'),
     };
 
     if (noChanges) {
@@ -136,28 +83,24 @@ const updateRtdb = async (body: ExpectedRequestBody) => {
 
 const postToGAS = (body: ExpectedRequestBody) => {
   if (!process.env.GAS_END_POINT) {
-    console.log("GAS WebアプリケーションのURLが登録されていません");
+    console.log('GAS WebアプリケーションのURLが登録されていません');
     return;
   }
   return fetch(process.env.GAS_END_POINT, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify({
       ...body,
-      from: "ribbit-next-app",
+      from: 'ribbit-next-app',
     }),
   });
 };
 
-const getNewProps = (
-  hostname: string,
-  body: ExpectedRequestBody,
-  options?: { rtdb?: boolean }
-) => {
+const getNewProps = (hostname: string, body: ExpectedRequestBody, options?: { rtdb?: boolean }) => {
   const date = body.installDate ? new Date(body.installDate) : new Date();
 
   return {
     hostname,
-    name: body.name || "",
+    name: body.name || '',
     counter: body.counter || 1,
     pluginNames: body.pluginNames || [],
     installDate: options?.rtdb ? date.toLocaleString() : date,
